051c24774e2c04343c0431e3da7921d411ae68ad
d-donia <d.donia@studenti.unisa.it>
2023-05-04 10:11:07 +0200

    Improved code quality by refactoring SetOperations, FixedOrderComparator, AbstractBidiMapTest, AbstractMapTest classes

--- src/test/java/org/apache/commons/collections4/map/AbstractMapTest.java
+++ src/test/java/org/apache/commons/collections4/map/AbstractMapTest.java
@@ -37,6 +37,7 @@ import org.apache.commons.collections4.CollectionUtils;
 import org.apache.commons.collections4.collection.AbstractCollectionTest;
 import org.apache.commons.collections4.keyvalue.DefaultMapEntry;
 import org.apache.commons.collections4.set.AbstractSetTest;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.openjdk.jmh.annotations.Scope;
 import org.openjdk.jmh.annotations.State;
@@ -1560,6 +1561,7 @@ public abstract class AbstractMapTest<K, V> extends AbstractObjectTest {
         return new TestMapEntrySet();
     }
 
+    @Nested
     public class TestMapEntrySet extends AbstractSetTest<Map.Entry<K, V>> {
         public TestMapEntrySet() {
             super("MapEntrySet");
